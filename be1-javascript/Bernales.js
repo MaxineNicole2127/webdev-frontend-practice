84540fcc9dd85558465dee27215605ac50af8632
juliette2127 <97998223+MaxineNicole2127@users.noreply.github.com>
2023-12-05 02:31:24 +0800

    Add: Bernales.js for be1-javascript project

---
+++ be1-javascript/Bernales.js
@@ -0,0 +1,40 @@
+class Entity {
+    constructor(name) {
+        this.name = name;
+        this.date = new Date().toDateString();
+    } walk() {
+        console.log(`${this.name} is walking`);
+    } birthdate() {
+        console.log(`${this.name} was created on ${this.date}`)
+    }
+}
+
+class Robot extends Entity {
+    constructor(name) {
+        super(name);
+        this.legs = 2;
+    } walk() {
+        console.log(`${this.name} is walking with legs--brrrrt`);
+    }
+}
+
+class Cat extends Entity {
+    constructor(name) {
+        super(name);
+        this.legs = 4;
+    } walk() {
+        console.log(`${this.name} is walking with legs--meow`);
+    } growl() {
+        return "Purrr~^^";
+    }
+}
+
+const robot = new Robot("Boboyboy");
+const cat = new Cat("Casper");
+robot.walk();
+cat.walk();
+
+console.log(`${robot.name} was created on ${robot.date}.`);
+console.log(`${cat.name} was created on ${cat.date}.`);
+
+console.log(`${cat.name} says "${cat.growl()}"`);
